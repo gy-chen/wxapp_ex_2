e484bc4ffd91749c06fbf07c14a851012b0dbfa1
GYCHEN <gy.chen@gms.nutc.edu.tw>
2017-05-19 16:19:34 +0800

    hello lifecycle 2

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,35 +1,30 @@
 //index.js
 Page({
   data: {
-    hello_onload: '',
-    hello_onshow: '',
-    hello_onready: '',
-    hello_onhide: '',
-    hello_onunload: ''
+    hellos: []
   },
-  onLoad: function() {
+
+  addHello: function(message) {
+    var hellos = this.data.hellos;
+    hellos.push(message);
     this.setData({
-      hello_onload: 'Hello onLoad 頁面載入'
+      hellos: hellos
     });
+  },
+
+  onLoad: function() {
+    this.addHello('Hello onLoad 頁面載入');
   },
   onShow: function() {
-    this.setData({
-      hello_onshow: 'Hello onShow 頁面顯示'
-    });
+    this.addHello('Hello onShow 頁面顯示');
   },
   onReady: function()  {
-    this.setData({
-      hello_onready: 'Hello onReady 頁面顯示完成'
-    });
+    this.addHello('Hello onReady 頁面顯示完成');
   },
   onHide: function() {
-    this.setData({
-      hello_onhide: 'Hello onHide 頁面隱藏'
-    });
+    this.addHello('Hello onHide 頁面隱藏');
   },
   onUnload: function() {
-    this.setData({
-      hello_onunload: 'Hello onUnload 頁面卸載'
-    });
+    this.addHello('Hello onUnload 頁面卸載');
   }
 })
